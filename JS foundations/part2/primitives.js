3501a49e4523d2f71ca17402b46a1684b9c5c130
jatinchhabraa <jchhabra_be22@thapar.edu>
2025-01-20 02:28:40 +0530

    progessing in JS

---
+++ JS foundations/part2/primitives.js
@@ -0,0 +1,37 @@
+let balalnce = 120;
+
+console.log(typeof balalnce); // number data type
+
+let anotherBalance = new Number(120);
+
+console.log(typeof anotherBalance); // object data type
+
+let isActive = true;
+
+console.log(typeof isActive); // bolean
+
+// null and undefined
+
+let firstName;
+console.log(firstName); // undefined
+
+let lastName = null;
+console.log(lastName); // null
+
+// string
+
+let myString = "hello";
+let myStringOne = "hola";
+let userName = "jatin";
+
+//////////////
+let greetMessage = `${myString} ${userName} !`; // string interpolation
+console.log(greetMessage);
+
+let demo = `value is ${2 ** 2}`;
+console.log(demo);
+
+// symbol guarantees to be uinique
+
+let sm1 = Symbol();
+console.log(sm1);
